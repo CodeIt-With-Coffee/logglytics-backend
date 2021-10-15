9077e3167f95bc0ca88aa29701bf294574370f4f
SanjayDevTech <sureshsanjay805@gmail.com>
2021-10-16 00:30:49 +0530

    added project route

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import express from "express";
 import { Request, Response } from "express";
 import cors from "cors";
 
+import projectRoute from "./routes/project";
+
 const app = express();
 
 app.use(cors());
 
 const { PORT = 3000 } = process.env;
+
+app.use("/project", projectRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
